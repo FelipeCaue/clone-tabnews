87d62e402f182db38bfa9b1c24065dcf3fb10686
FelipeCaue <felipecaue.fraga@gmail.com>
2024-11-23 00:36:06 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-function Home(){
-    return <h1>Jorge e Isadora vocês são os melhores filhos do mundo</h1>
+function Home() {
+  return <h1>Jorge e Isadora vocês são os melhores filhos do mundo</h1>;
 }
-
 export default Home;
